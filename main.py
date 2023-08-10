4db4242e1e3df14ef869cde1fe93585df0ab5ee0
Athe-kunal <athekunal@gmail.com>
2023-08-10 00:17:14 -0700

    Summarization Fixes

--- main.py
+++ main.py
@@ -8,3 +8,6 @@ if __name__ == "__main__":
     final_summary = generate_summary(ticker,year,filing_type)
     print(final_summary)
     
+
+
+    
--- summarize.py
+++ summarize.py
@@ -30,18 +30,19 @@ query_str = """
     Can you provide a comprehensive summary of the given text? The summary should cover all the key points and numerical figures presented in the original text, while also condensing the information.
 """
 
-async def get_response(doc:str,query_str:str,chunk_size:int=1024,chunk_overlap:int=128,verbose:bool=True,use_async:bool=True):
+def get_response(doc:str,query_str:str,chunk_size:int=1024,chunk_overlap:int=128,verbose:bool=True,use_async:bool=True):
     service_context = ServiceContext.from_defaults(chunk_size=chunk_size,chunk_overlap=chunk_overlap)
     summarizer = TreeSummarize(verbose=verbose,service_context=service_context,use_async=use_async)
         
-    response = await summarizer.aget_response(query_str,[doc])
+    response = summarizer.get_response(query_str,[doc])
     return response
 
-def get_summary(docs:List[str],metadata:List[dict],filing_type:str="10-K"):
-    summary=""
+def get_summary(docs:List[str],metadata:List[dict],ticker,year,filing_type:str="10-K"):
     if filing_type=="10-K":ALL_SECTIONS = SECTIONS_10K
     elif filing_type=="10-Q":ALL_SECTIONS = SECTIONS_10Q
+    full_summary = ""
     for section in ALL_SECTIONS:
+        summary=""
         #Give me the idxs in splitted metadata for which we have this section
         
         sections_docs = ""
@@ -59,24 +60,33 @@ def get_summary(docs:List[str],metadata:List[dict],filing_type:str="10-K"):
             summary+=" ".join(section.split("_")) + "\n"
             summary+=""
             summary+="\n\n"
+            full_summary+=summary
+            with open(f'{ticker}_{year}_{filing_type}.txt', 'a') as f:
+                f.write(summary)
         elif section_amended_docs=="" and sections_docs!="":
             response = get_response(sections_docs,query_str)
             summary+=" ".join(section.split("_")) + "\n"
             summary+=response
             summary+="\n\n"
+            full_summary+=summary
+            with open(f'{ticker}_{year}_{filing_type}.txt', 'a') as f:
+                f.write(summary)
         elif section_amended_docs!="" and sections_docs!="":
             section_response = get_response(sections_docs,query_str)
-            section_amended_response = get_response(section_amended_response,query_str)
+            section_amended_response = get_response(section_amended_docs,query_str)
             summary+=" ".join(section.split("_")) + "\n"
             summary+=section_response +"\n\n"
             summary+="Amended Section: \n"+section_amended_response
             summary+="\n\n"
-    return summary
+            full_summary+=summary
+            with open(f'{ticker}_{year}_{filing_type}.txt', 'a') as f:
+                f.write(summary)
+    return full_summary
 
 def generate_summary(ticker:str,year:str,filing_type:str):
-    docs,metadata = load_documents("TSLA","2021","10-K")
-    final_summary = get_summary(docs,metadata,filing_type)
+    docs,metadata = load_documents(ticker,year,filing_type)
+    final_summary = get_summary(docs,metadata,ticker,year,filing_type)
 
-    return generate_summary
+    return final_summary
 
 
